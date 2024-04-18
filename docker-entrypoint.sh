6879c63104c68727af9166cb96456dae6a3a6a93
HA5KFU <ha5kfu@simonyi.bme.hu>
2024-04-18 11:20:46 +0200

    Better docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,14 +2,5 @@
 cd /opt/app/statusch_backend || exit
 python manage.py migrate                  # Apply database migrations
 python manage.py collectstatic --noinput  # Collect static files
-
-# Start Gunicorn processes
-echo Starting Gunicorn.
-gunicorn statusch.wsgi:application \
-    --name statusch \
-    --bind 0.0.0.0:8000 \
-    --workers 10 \
-    --timeout 120 \
-    --log-level=info
-
+python manage.py runserver
 "$@"
